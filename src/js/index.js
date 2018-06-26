b9397f9a9f16f569f05c2d058e6291e920dc5b91
randomlysa <su@randomlysa.com>
2018-06-26 14:15:45 -0500

    getResults - fetch recipes function

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,19 @@
+import axios from 'axios';
+
+import { food2forkAPIKey as key } from '../../api.js';
+
 // Global app controller
+
+async function getResults(query) {
+  const proxy = 'https://cors-anywhere.herokuapp.com/';
+
+  try {
+    const result = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
+    const recipes = result.data.recipes;
+    console.log(recipes);
+  } catch (e) {
+    alert(e);
+  } // catch
+} // getResults
+
+getResults('pasta');
