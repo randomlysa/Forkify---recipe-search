c86b9366d5622e9f5a5b55f7796e3a3cbd9e0e13
randomlysa <su@randomlysa.com>
2018-06-26 17:01:07 -0500

    addEventListener to search
    when search is clicked, search for 'pizza'

--- src/js/index.js
+++ src/js/index.js
@@ -8,3 +8,26 @@ import Search from './models/Search';
  * - Liked recipes
 */
 const state = {};
+
+const controlSearch = async () => {
+  // Get the query from the view.
+  const query = 'pizza'; // TODO
+
+  if (query) {
+    // Create a new search object and add it to state.
+    state.search = new Search(query);
+
+    // Prepare the UI for results.
+
+    // Search for recipes.
+    await state.search.getResults();
+
+    // Render results on the UI.
+    console.log(state.search.result);
+  }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+});
