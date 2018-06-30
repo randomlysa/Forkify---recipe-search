b658d80d93cd13ac634dedc2ac771700b5f00b90
randomlysa <su@randomlysa.com>
2018-06-30 15:47:38 -0500

    get text input into search__field

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 // Global app controller
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
 /** Global state of the app
  * - Search object
@@ -11,7 +13,8 @@ const state = {};
 
 const controlSearch = async () => {
   // Get the query from the view.
-  const query = 'pizza'; // TODO
+  const query = searchView.getInput();
+  console.log(query);
 
   if (query) {
     // Create a new search object and add it to state.
@@ -27,7 +30,7 @@ const controlSearch = async () => {
   }
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
   e.preventDefault();
   controlSearch();
 });
---
+++ src/js/views/searchView.js
@@ -0,0 +1,5 @@
+import { elements } from './base';
+
+// One line arrow function = implicit return.
+export const getInput = () => elements.searchInput.value;
+
