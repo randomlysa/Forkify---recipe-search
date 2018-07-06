2ec5b00059755465bb4dbf66fcde4abc25e319ea
randomlysa <su@randomlysa.com>
2018-07-05 21:27:07 -0500

    increase / decrease servings

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -65,12 +65,12 @@ export const renderRecipe = recipe => {
             <span class="recipe__info-text"> servings</span>
 
             <div class="recipe__info-buttons">
-                <button class="btn-tiny">
+                <button class="btn-tiny btn-decrease">
                     <svg>
                         <use href="img/icons.svg#icon-circle-with-minus"></use>
                     </svg>
                 </button>
-                <button class="btn-tiny">
+                <button class="btn-tiny btn-increase">
                     <svg>
                         <use href="img/icons.svg#icon-circle-with-plus"></use>
                     </svg>
@@ -113,4 +113,15 @@ export const renderRecipe = recipe => {
   `;
 
   elements.recipe.insertAdjacentHTML('afterbegin', markup);
+};
+
+export const updateServingsIngredients = recipe => {
+  // Update servings.
+  document.querySelector('.recipe__info-data--people').textContent = recipe.servings;
+
+  // Update ingredients.
+  const countElements = Array.from(document.querySelectorAll('.recipe__count'));
+  countElements.forEach((item, index) => {
+    item.textContent = formatCount(recipe.ingredients[index].count);
+  });
 };
