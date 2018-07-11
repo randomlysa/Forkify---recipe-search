20e75ee06d08b4af18be1f9aa5e209e43e3ae16e
randomlysa <su@randomlysa.com>
2018-07-11 16:37:59 -0500

    save likes when recipe is added or deleted

--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -6,12 +6,20 @@ export default class Likes {
   addLike(id, title, author, img) {
     const like = {id, title, author, img};
     this.likes.push(like);
+
+    // Save data to localStorage.
+    this.saveDataToLocalStorage();
+
     return like;
   }
 
   deleteLike(id) {
     const index = this.likes.findIndex(item => item.id === id);
     this.likes.splice(index, 1);
+
+    // Save data to localStorage.
+    this.saveDataToLocalStorage();
+
   } // deleteLike
 
   isLiked(id) {
