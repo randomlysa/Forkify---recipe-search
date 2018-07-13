76003273cc5162790857a7d27788d84e4d9bab40
randomlysa <su@randomlysa.com>
2018-07-13 11:39:47 -0500

    improve recipe text/ingredient parsing
    id 47744, lines 'roughly 3/4 cup' and 'a few tbps of', were causing errors and the recipe wouldn't load

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -42,7 +42,13 @@ export default class Recipe {
     const unitsShort = ['tbps', 'tbps', 'oz', 'oz', 'tsp', 'tsp', 'cup',
       'pound', 'kg', 'g'];
 
+    // Search for 'one' and replace with '1', etc.
+    const numbersInWords = ['one', 'two', 'three', 'four', 'five', 'six',
+      'seven', 'eight', 'nine', 'ten'];
+    const numbersInInt = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
     const newIngredients = this.ingredients.map(item => {
+
       // Uniform units.
       let ingredient = item.toLowerCase();
       unitsLong.forEach((unit, index) => {
@@ -52,21 +58,29 @@ export default class Recipe {
       // Remove parenthesis.
       ingredient = ingredient.replace(/ *\([^)]*\) */g, ' ');
 
-      // Parse ingredients into count, unit, and ingredient.
+      // Used to parse ingredients into count, unit, and ingredient.
       const arrIng = ingredient.split(' ');
+
+      // Convert word numbers to int. Works with id 47744.
+      // Example: one small handful of raw pine nuts --> 1 small handful...
+      const wordNumberIndex = arrIng.findIndex(item3 => numbersInWords.includes(item3));
+      if (wordNumberIndex > -1 ) arrIng[wordNumberIndex] = numbersInInt[wordNumberIndex];
+
+      // Parse ingredients into count, unit, and ingredient.
       const unitIndex = arrIng.findIndex(item2 => unitsShort.includes(item2));
 
       let objectIngredient;
-      if (unitIndex > -1) {
-        // There is a unit.
+      if (unitIndex > -1 && wordNumberIndex > -1) {
+        // CASE 1 ------------ There is a unit.
         // 4 1/2 cups --- arrayCount =  [4, 1/2].
         // 4 cups     --- arrayCount =  [4].
         const arrayCount = arrIng.slice(0, unitIndex);
         let count;
+
         if (arrayCount.length === 1) {
           count = eval(arrIng[0].replace('-', '+'));
         } else {
-          count = eval(arrIng.slice(0, unitIndex).join('+'));
+          count = eval(arrIng.slice(unitIndex - 1, unitIndex).join('+'));
         }
 
         objectIngredient = {
@@ -75,20 +89,29 @@ export default class Recipe {
           ingredient: arrIng.slice(unitIndex + 1).join(' ')
         };
       } else if (parseInt(arrIng[0], 10)) {
-        // There is no unit but first element is a number.
+        // CASE 2 ------------ There is no unit but first element is a number.
         objectIngredient = {
           count: parseInt(arrIng[0], 10),
           unit: '',
           ingredient: arrIng.slice(1).join(' ')
         };
       } else if (unitIndex === -1) {
-        // There is no unit.
+        // CASE 3 ------------ There is no unit.
         objectIngredient = {
           count: 1,
           unit: '',
           ingredient
         };
-      }
+      } else {
+        // CASE 4 ------------ There is a unit but no number,
+        //                     or some other situation. (ID: 47744)
+        objectIngredient = {
+          count: '',
+          unit: '',
+          ingredient
+        };
+      } // else
+
       return objectIngredient;
     }); // this.ingredients.map
 
