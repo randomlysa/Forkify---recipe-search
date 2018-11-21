b0caaeff4c97cc7ca82ad2d32a5292a86f08287c
randomlysa <su@randomlysa.com>
2018-11-21 10:45:52 -0600

    update active menu item when swiping

--- src/js/utils/swiper.js
+++ src/js/utils/swiper.js
@@ -18,33 +18,41 @@ export const setupSwiper = () => {
     initialSlide: initialSlide
   });
 
-  const goTo = (slide) => {
-    state.currentSlide = slide;
-    mySwiper.slideTo(slide);
+  // Update active menu item when swiping.
+  mySwiper.on('slideChange', () => {
+    updateActiveMenuItem(mySwiper.activeIndex);
+  })
 
+  // Switch slides. Runs when a recipe is searched for (goTo 0) or when a recipe
+  // is clicked (goTo 1.)
+  const goTo = (slide) => {
+    mySwiper.slideTo(slide);
+    updateActiveMenuItem(slide);
+  }; // const goTo = (slide) =>
+
+  // Update active menu item. Currently puts a dark yellow border under it.
+  const updateActiveMenuItem = (activeSlide) => {
     // Select all recipe links.
     const allMenuLinks = Array.from(document.querySelectorAll(`.breadcrumb__title`));
 
     // Remove active class from all.
     allMenuLinks.forEach(link => link.classList.remove('breadcrumb--active'));
 
-    // Add active class to link that has a data-menu = state.currentSlide.
+    // Add active class to link that has a data-menu = activeSlide.
     const currentLink = document.querySelector(
-      `.breadcrumb__title[data-menu='${state.currentSlide}']`
+      `.breadcrumb__title[data-menu='${activeSlide}']`
     );
-    // currentLink doesn't exist if the recipe id doesn't exist.
+    // currentLink should always exist but check anyway.
     if (currentLink) currentLink.classList.add('breadcrumb--active');
-  }; // const goTo = (slide) =>
+  };
 
   // Set up click handling on menu.
   base.elements.showResults.addEventListener('click', () => goTo(0));
   base.elements.showRecipe.addEventListener('click', () => goTo(1));
   base.elements.showCart.addEventListener('click', () => goTo(2));
 
-  // Use goTo to mark item in menu. The page should have already loaded on this
-  // slide.
-  if (id) goTo(1);
-  else goTo(0);
+  // Mark the menu item for the slide the page loaded on.
+  updateActiveMenuItem(initialSlide);
 
   // Add goTo to state.
   state.goTo = goTo;
